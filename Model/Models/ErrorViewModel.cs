b4fae6504520a31b8bb4ab327e63a30163b3f0b5
keksikls <151632212+keksikls@users.noreply.github.com>
2024-11-05 16:35:40 +0300

    n-tier-architecture(мн.ар.)
    n-tier-architecture(мн.ар.)

---
+++ Model/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace n_tier_architecture__многослояная_архитектура_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
